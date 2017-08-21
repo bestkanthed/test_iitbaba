7d079ae1b4391dece8c6138e19462079c61bfa49
kanthed <bestkanthed@gmail.com>
2017-08-21 18:32:10 +0530

    Delete

--- services/notifications.js
+++
@@ -1,8 +0,0 @@
-const Notification = require('../models/Notification');
-
-exports.getNotifications = (ldap ,no_of_notifications, sort)=> {
-    Notification.find({ldap: ldap},{},{sort:{'createdAt': sort}}).limit(no_of_notifications).exec((err, noti)=>{
-        if (err) { return next(err); reject(err); }
-        return noti;
-    });
-}
